c661922f7500af4f277a9ff6192e6cf83a40ca5a
Spacecode10 <103993621+Spacecode10@users.noreply.github.com>
2026-08-21 21:43:14 +0530

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int temp = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] == 1)
+            {
+                temp++;
+            }
+            else
+            {
+                count = Math.max(temp,count);
+                temp = 0;
+            }
+        }
+        return Math.max(temp,count);
+    }
+}
